80798b2d90416220d66483219253c1a16c58dd7e
Tomé Ferreira <tomepereiraferreira@gmail.com>
2025-06-09 16:05:43 +0100

    Updated/Created Client area

--- src/layouts/ClientLayout.tsx
+++ src/layouts/ClientLayout.tsx
@@ -2,16 +2,38 @@ import { Outlet, Navigate, useLocation } from 'react-router';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { PageContainer } from '@toolpad/core/PageContainer';
 import { useSession } from '../contexts/SessionContext';
+import { ReactRouterAppProvider } from '@toolpad/core/react-router';
+import { smartHiveMainTheme } from '../services/themeService';
+import { Navigation } from '@toolpad/core';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+
+const NAVIGATION: Navigation = [
+  {
+    segment: 'Client/Dashboard',
+    title: 'Dashboard',
+    icon: <DashboardIcon />,
+  }
+];
 
 export default function ClientLayout() {
   const location = useLocation();
 
   return (
-    <DashboardLayout>
-      <PageContainer>
-        <Outlet />
-      </PageContainer>
-    </DashboardLayout>
+    <ReactRouterAppProvider
+      navigation={NAVIGATION}
+      branding={{
+        logo: <img src="/img/logos/SmartHiveLogoHor.png" alt="Smart Hive logo" />,
+        title: '',
+        homeUrl: '/',
+      }}
+      theme={smartHiveMainTheme}
+    >
+      <DashboardLayout>
+        <PageContainer>
+          <Outlet />
+        </PageContainer>
+      </DashboardLayout>
+    </ReactRouterAppProvider>
   );
 }
   
--- src/pages/ClientGroup/UserDashboard.tsx
+++ src/pages/ClientGroup/UserDashboard.tsx
@@ -5,20 +5,18 @@ const UserDashboard = () => {
   const { user, signOut } = useAuth();
 
   return (
-    <div className="min-h-screen flex items-center justify-center smarthive-grad-bg p-10">
-      <div className="bg-[#f0f9f4] p-8 rounded-lg shadow-lg text-center">
-        <h1 className="text-2xl font-bold mb-4">Hello, you're authenticated!</h1>
-        {user ? (
-          <>
-            <p><strong>Email:</strong> {user.email}</p>
-          </>
-        ) : (
-          <p>Loading user info...</p>
-        )}
+    <>
+          <h1 className="text-2xl font-bold mb-4">Hello, you're authenticated!</h1>
+            {user ? (
+              <>
+                <p><strong>Email:</strong> {user.email}</p>
+              </>
+            ) : (
+              <p>Loading user info...</p>
+            )}
 
         <SmartHivePrimaryBtn text="Log-out" onClick={signOut} />
-      </div>
-    </div>
+    </>
   );
 };
 
